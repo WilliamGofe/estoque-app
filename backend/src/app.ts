9351ce4fa0127f1301f7dca57b2ca1141ef628e6
William Gomes Ferreira <wf120778@grpoesp.com.br>
2025-08-25 19:29:16 -0300

    feat: dependencias da arquitetura

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import * as express from "express";
-import * as cors from "cors";
+import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
 import stockRoutes from "./routes/stockRoutes";
